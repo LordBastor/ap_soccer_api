531f785a6aa2b5185dc7bfa49665c1c6e566eb07
lordbastor <lordbastor@gmail.com>
2019-12-18 18:18:32 +0200

    Payment invoice number

--- src/payments/models.py
+++ src/payments/models.py
@@ -6,3 +6,6 @@ class Payment(models.Model):
     invoice_url = models.CharField(max_length=255)
     amount_due = models.DecimalField(max_digits=12, decimal_places=2)
     amount_paid = models.DecimalField(max_digits=12, decimal_places=2)
+
+    def __str__(self):
+        return self.invoice_number
