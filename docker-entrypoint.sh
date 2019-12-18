c06afc5a07573e18e7e82518b04825ce70268c66
lordbastor <lordbastor@gmail.com>
2019-12-18 14:17:13 +0200

    First commit

---
+++ docker-entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+if [ "$ENV" = "development" ] ; then
+    python docker/web/check_db.py --service-name postgres --ip db --port 5432
+fi
+
+if [ "$1" = "manage" ]; then
+    shift 1
+    exec python src/manage.py "$@"
+else
+    python src/manage.py migrate                  # Apply database migrations
+    python src/manage.py collectstatic --noinput  # Collect static files
+
+    # Prepare log files and start outputting logs to stdout
+    mkdir -p /srv/logs/
+    touch /srv/logs/gunicorn.log
+    touch /srv/logs/access.log
+    tail -n 0 -f /srv/logs/*.log &
+
+    # Start Gunicorn processes
+    echo Starting Gunicorn
+    exec gunicorn app.wsgi \
+        --bind 0.0.0.0:8000 \
+        --chdir /usr/src/app/src \
+        --workers 3 \
+        --log-level=info \
+        --log-file=/srv/logs/gunicorn.log \
+        --access-logfile=/srv/logs/access.log
+fi
