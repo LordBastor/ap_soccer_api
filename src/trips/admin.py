1801aaa1b43e376b8dad2138976e243e1cb02dfd
lordbastor <lordbastor@gmail.com>
2019-12-18 15:53:20 +0200

    Trip migration

--- src/trips/admin.py
+++ src/trips/admin.py
@@ -1,3 +1,24 @@
-from django.contrib import admin
+from django.contrib import admin, forms
 
-# Register your models here.
+from .models import Package, Trip
+
+
+class PackageAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Package, PackageAdmin)
+
+
+class TripAdminForm(forms.ModelAdmin):
+    def clean_live(self):
+        import ipdb
+
+        ipdb.set_trace()
+
+
+class TripAdmin(admin.ModelAdmin):
+    form = TripAdminForm
+
+
+admin.site.register(Trip, TripAdmin)
--- src/trips/models.py
+++ src/trips/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Package(models.Model):
+    name = models.CharField(max_length=30)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    description = models.TextField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return "{} - ${}".format(self.name, self.price)
+
+
+class Trip(models.Model):
+    name = models.CharField(max_length=255, blank=False)
+    from_date = models.DateTimeField(blank=True)
+    to_date = models.DateTimeField(blank=True)
+    live = models.BooleanField(default=False)
+    player_price = models.DecimalField(max_digits=7, decimal_places=2)
+    traveler_price = models.DecimalField(max_digits=7, decimal_places=2)
+    package_options = models.ManyToManyField(Package)
+
+    def __str__(self):
+        return "{} from {} to {}".format(
+            self.location, self.from_date.date(), self.to_date.date()
+        )
+
+
+class TripInvitation(models.Model):
+    pass
