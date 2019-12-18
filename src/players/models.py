1d3ece486bb547cb882b0a0d5fb44f93ef837499
lordbastor <lordbastor@gmail.com>
2019-12-18 16:44:51 +0200

    Player data

--- src/players/models.py
+++ src/players/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class Player(models.Model):
+    GOALKEEPER = "Goalkeeper"
+    DEFENSE = "Defense"
+    MIDFIELD = "Midfield"
+    FORWARD = "Forward"
+    POSITION_CHOICES = (
+        (GOALKEEPER, "Goalkeeper"),
+        (DEFENSE, "Defense"),
+        (MIDFIELD, "Midfield"),
+        (FORWARD, "Forward"),
+    )
+
+    MALE = "Male"
+    FEMALE = "Female"
+    GENDER_CHOICES = ((MALE, "Male"), (FEMALE, "Female"))
+
+    parent_name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    position = models.CharField(max_length=10, choices=POSITION_CHOICES)
+    gender = models.CharField(max_length=6, choices=GENDER_CHOICES)
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    country = models.CharField(max_length=255, default="United States")
+    zip_code = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    medical_conditions = models.TextField()
+    emergency_contact = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name + self.position
