60f353de1745cb6391207c89da51294f09f0d76f
Boris Gaganelov <boris@infinitelambda.com>
2022-05-28 21:31:08 +0300

    Trip invitation filter

--- src/trips/admin.py
+++ src/trips/admin.py
@@ -69,6 +69,8 @@ class TripInvitationAdmin(admin.ModelAdmin):
 
     list_display = ("document", "trip_invitation", "created_date")
 
+    list_filter = ("trip_invitation",)
+
 
 admin.site.register(TripInvitationFile, TripInvitationAdmin)
 
